0efb6756619cd9959675231ea79725f41b73e765
Zoran <heinczingerz@5net.hu>
2017-02-26 11:15:07 +0100

    Initialize project

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+
+<html>
+    <head>
+        <title>Misto Bistro - Ahol a hazai konyha, és a mediterrán ízek találkoznak</title>
+
+        <!--Style-->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css">
+
+        <!--JavaScripts-->
+        <script src="assets/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        Teszt
+    </body>
+</html>
